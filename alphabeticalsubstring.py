771b248ee846c31721c55f5934ba799ece70c85e
Mildred 2 <milydemendoza@gmail.com>
2018-12-18 12:44:08 +0100

    uploading files

---
+++ alphabeticalsubstring.py
@@ -0,0 +1,18 @@
+s = 'azcbobobegghakl'
+
+sub = ""
+submax = ""
+
+for i in range(0, len(s)-1):
+
+    if s[i] <= s[i+1]:
+        if sub == "":
+            sub = s[i] + s[i+1]
+        else:
+            sub = sub + s[i+1]
+    elif s[i] > s[i+1]:
+        sub = ""
+
+    if len(sub) > len(submax):
+        submax = sub
+print(submax)
